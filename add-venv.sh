f9b26824bea4ee5125e651b404960c12c1425e28
David Northover <david.northover@temple.edu>
2017-05-17 16:18:02 -0400

    Basic Configuration Updates

--- add-venv.sh
+++ add-venv.sh
@@ -1,18 +1,23 @@
 # Python3 and virtualenv should already be installed.
-# Deployment using fabric?
 # May need to add python3-venv / python3-pip if missing.
 
 # First, create the virtual enviornment and install packages.
 if [ ! -d "/usr/local/venvs" ]; then
 	mkdir /usr/local/venvs
 fi
-/usr/bin/python3 -m venv /usr/local/venvs/taed
+echo "Enter Binary Path"
+read binary
+
+$binary/virtualenv -p $binary/python3 /usr/local/venvs/taed
+
 . /usr/local/venvs/taed/bin/activate
-pip3 install --upgrade setuptools
-pip3 install jsonpickle
-pip3 install flask
-pip3 install biopython
-pip3 install ruamel.yaml
-pip3 install mysqlclient
-pip3 install requests
+pip install --upgrade setuptools
+pip install numpy
+pip install jsonpickle
+pip install flask
+pip install biopython
+pip install ruamel.yaml
+pip install mysqlclient
+pip install requests
+pip install fabric3
 deactivate
