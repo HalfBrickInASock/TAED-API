63145281cb2c7cba79f428b0a24687048120c4b1
David Northover <david.northover@temple.edu>
2017-05-17 16:30:39 -0400

    * Conf Changes

--- local/localrun.sh
+++ local/localrun.sh
@@ -6,7 +6,7 @@ export FLASK_APP=TAED_API
 export FLASK_DEBUG=true
 
 . /usr/local/venvs/taed/bin/activate
-pip install -e .
+pip install -e ..
 cp TAED_API/config.yaml /usr/local/venvs/taed/.
 cd /usr/local/venvs/taed
 flask run
